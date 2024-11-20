1df8b78f5fd87ac6c5b3a9cf7efe4c7010c32051
Shyam Kumbhar <shyamkumbhar509@gmail.com>
2024-11-20 22:32:18 +0530

    Create Models define Relationship

---
+++ app/Models/FamilyHead.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FamilyHead extends Model
+{
+    use HasFactory;
+
+    protected $table = 'family_heads';
+
+    protected $fillable = [
+        'name',
+        'surname',
+        'birthdate',
+        'mobile_no',
+        'address',
+        'state',
+        'city',
+        'pincode',
+        'marital_status',
+        'wedding_date',
+        'hobbies',
+        'photo',
+    ];
+
+    // Define the relationship with FamilyMember
+    public function familyMembers()
+    {
+        return $this->hasMany(FamilyMember::class);
+    }
+}
---
+++ app/Models/FamilyMember.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FamilyMember extends Model
+{
+    use HasFactory;
+
+    protected $table = 'family_members';
+
+    protected $fillable = [
+        'family_head_id',
+        'name',
+        'birthdate',
+        'marital_status',
+        'wedding_date',
+        'education',
+        'photo',
+    ];
+
+    // Define the relationship with FamilyHead
+    public function familyHead()
+    {
+        return $this->belongsTo(FamilyHead::class);
+    }
+}
